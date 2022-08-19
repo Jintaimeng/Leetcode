e13ed764a3a0dc4953c470b8735495523d63b45a
929357244@qq.com <929357244@qq.com>
2022-08-19 16:49:06 +0800

    commit

--- 递归、排序、贪心/56.py
+++ 递归、排序、贪心/56.py
@@ -5,7 +5,7 @@ class Solution:
             return intervals
         intervals.sort()
         res = [intervals[0][0], intervals[0][1]]
-        for i in range(len(intervals)):
+        for i in range(1, len(intervals)):
             if res[1] >= intervals[i][0] and res[1] < intervals[i][1]:
                 res = [res[0], intervals[i][1]]
             elif res[1] < intervals[i][0]:
