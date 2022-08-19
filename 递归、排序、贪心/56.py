e31e8249b03c8967c28054c2491476b7d9f53faf
929357244@qq.com <929357244@qq.com>
2022-08-19 10:58:09 +0800

    commit

---
+++ 递归、排序、贪心/56.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = list()
+        if len(intervals) == 1:
+            return intervals
+        intervals.sort()
+        res = [intervals[0][0], intervals[0][1]]
+        for i in range(len(intervals)):
+            if res[1] >= intervals[i][0] and res[1] < intervals[i][1]:
+                res = [res[0], intervals[i][1]]
+            elif res[1] < intervals[i][0]:
+                result.append(res)
+                res = [intervals[i][0], intervals[i][1]]
+            if i == len(intervals) - 1:
+                result.append(res)
+        return result
