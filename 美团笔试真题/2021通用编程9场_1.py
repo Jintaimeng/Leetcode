4fc9be61234801183127e7416993c5f1e2b823a3
Jintaimeng <929357244@qq.com>
2023-03-23 14:50:28 +0800

    meituan1

---
+++ 美团笔试真题/2021通用编程9场_1.py
@@ -0,0 +1,18 @@
+while True:
+    try:
+        n, m, a, b = map(int, input().split(" "))
+        if a > b:
+            a, b = b, a
+        maked_list = list(map(int, input().split(" ")))
+        maked_list.sort()
+        now_min = maked_list[0]
+        now_max = maked_list[-1]
+        if a == now_min and b == now_max:
+            print("YES")
+        elif (a < now_min and b == now_max and n > m) or (a == now_min and b >now_max and n > m) :
+            print("YES")
+        elif a < now_min and b > now_max and n-m >= 2:
+            print("YES")
+        else:
+            print("NO")
+    except: break
