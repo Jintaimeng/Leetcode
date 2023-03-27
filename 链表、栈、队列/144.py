131ad7a98bfae658b2a0f5da5e0b20521c2f44a0
Jintaimeng <929357244@qq.com>
2023-03-27 19:09:41 +0800

    94/144/145

---
+++ 链表、栈、队列/144.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorder(self, root):
+        if root is None:
+            return
+        self.res.append(root.val)
+        self.preorder(root.left)
+        self.preorder(root.right)
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.res = []
+        self.preorder(root)
+        return self.res
