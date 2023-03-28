6cc997c89f4d54123f42d763e64758e46647c338
Jintaimeng <929357244@qq.com>
2023-03-28 19:16:58 +0800

    102/107

---
+++ 链表、栈、队列/102.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        results = []
+        if not root:
+            return results
+        queue = deque()
+        queue.append(root)
+        while queue:
+            temp = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            results.append(temp)
+        return results
---
+++ 链表、栈、队列/107.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        if not root:
+            return res
+        queue = deque()
+        queue.insert(0, root)
+        while queue:
+            temp = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.insert(0, temp)
+        return res
