21bf09fa0a0214c592ca1cf1adbc52ffe55c36d8
Jintaimeng <929357244@qq.com>
2023-03-28 23:38:23 +0800

    94/144/145

--- 链表、栈、队列/145_解2.py
+++ 链表、栈、队列/145_解2.py
@@ -4,25 +4,22 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-        if root is None:
+        if not root:
             return res
         stack = deque()
-        n = ("go", root)
-        stack.append(n)
-        while len(stack) > 0:
+        stack.append(("go", root))
+        while stack:
             command = stack.pop()
             if command[0] == "print":
                 res.append(command[1].val)
             elif command[0] == "go":
-                if command[1].right:
-                    node_right = ("go", command[1].right)
-                    stack.append(node_right)
-                if command[1].left:
-                    node_left = ("go", command[1].left)
-                    stack.append(node_left)
                 stack.append(("print", command[1]))
+                if command[1].right:
+                    stack.append(("go", command[1].right))
+                if command[1].left:
+                    stack.append(("go", command[1].left))
         return res
-
