1a9ff8d61278510c23b1d148af50f050af27d198
Jintaimeng <929357244@qq.com>
2023-03-29 12:48:02 +0800

    455

--- 递归、排序、贪心/455.py
+++ 递归、排序、贪心/455.py
@@ -1,11 +1,15 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        g.sort()
-        s.sort()
+        g.sort(reverse=True)
+        s.sort(reverse=True)
         child = 0
         cookie = 0
+        res = 0
         while child < len(g) and cookie < len(s):
             if s[cookie] >= g[child]:
                 child += 1
-            cookie += 1
-        return child
+                cookie += 1
+                res += 1
+            else:
+                child += 1
+        return res
