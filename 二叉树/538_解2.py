e59b399e3e237fe70e324f0e7ad2c0823bdd9d60
Jintaimeng <929357244@qq.com>
2023-04-01 15:20:19 +0800

    538

---
+++ 二叉树/538_解2.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        stack = []
+        node = root
+        total = 0
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.right
+            node = stack.pop()
+            total += node.val
+            node.val = total
+
+            node = node.left
+        return root
