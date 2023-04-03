5ddb1f87e6c2b5572fc1d4c683c29cbbe36ae123
Jintaimeng <929357244@qq.com>
2023-04-03 16:27:28 +0800

    77

---
+++ 搜索算法、回溯算法、位运算、二分查找/77.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.res = []
+        self.temp = []
+        self.combine(n, k, 1)
+        return self.res
+
+    def combineT(self, n, k, start):
+        if len(self.temp) == k:
+            self.res.append(self.temp[0: ])
+            return
+        for i in range(start, n+1):
+            self.temp.append(i)
+            self.combineT(n, k, i+1)
+            self.temp.pop()
