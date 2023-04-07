242233d27f25921046880f2ceae8226243861b21
Jintaimeng <929357244@qq.com>
2023-04-07 21:41:46 +0800

    70

---
+++ 动态规划、背包问题/70.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1 or n == 2:
+            return n
+        memo = [-1 for _ in range(n + 1)]
+        memo[1] = 1
+        memo[2] = 2
+        if memo[n] != -1:
+            return memo[n]
+        for i in range(3, n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        return memo[n]
