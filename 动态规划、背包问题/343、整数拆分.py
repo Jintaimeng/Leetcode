066a2c678264188045ca3b56aabe63e7c6579a97
Jintaimeng <929357244@qq.com>
2023-08-04 19:50:56 +0800

    343

---
+++ 动态规划、背包问题/343、整数拆分.py
@@ -0,0 +1,20 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        memo = [-1 for _ in range(n + 1)]
+        memo[0] = 0
+        memo[1] = 0
+        memo[2] = 1
+        for i in range(3, n + 1):
+            for j in range(1, n):
+                memo[i] = max(j * (i - j), j * memo[i - j], memo[i])
+        return memo[n]
+
+
+def main():
+    n = 2
+    res = Solution().integerBreak(n)
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
