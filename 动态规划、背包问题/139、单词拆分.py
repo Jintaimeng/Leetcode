0f6131def4dba28d3acfe2e8f1cb2083ff04b44a
Jintaimeng <929357244@qq.com>
2023-08-07 18:15:04 +0800

    139

---
+++ 动态规划、背包问题/139、单词拆分.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = [False for _ in range(len(s) + 1)]
+        memo[0] = True
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if s[j: i] in wordDict and memo[j]:
+                    memo[i] = True
+        return memo[len(s)]
+
+
+
+def main():
+    s = "leetcode"
+    wordDict = ["leet", "code"]
+    res = Solution().wordBreak(s, wordDict)
+    print(res)
+
+if __name__ == "__main__":
+    main()
