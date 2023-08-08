ad33475c615ddd2f85a242ceba746be762b467ec
Jintaimeng <929357244@qq.com>
2023-08-08 10:53:19 +0800

    198

--- 动态规划、背包问题/198、打家劫舍.py
+++ 动态规划、背包问题/198、打家劫舍.py
@@ -3,13 +3,21 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-
+        if len(nums) == 1:
+            return nums[0]
+        memo = [0 for _ in range(len(nums))]
+        memo[0] = nums[0]
+        memo[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            memo[i] = max(memo[i - 2] + nums[i], memo[i - 1])
+        return memo[-1]
 
 
 def main():
-    nums = [1,2,3,1]
+    nums = [2,7,9,3,1]
     res = Solution().rob(nums)
     print(res)
+
 
 if __name__ == "__main__":
     main()
