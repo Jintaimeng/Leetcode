abde0f75c885cec200119530482541e2935e6983
Jintaimeng <929357244@qq.com>
2023-08-16 22:38:41 +0800

    583—_2

--- 动态规划、背包问题/583、两个字符串的删除操作_2.py
+++ 动态规划、背包问题/583、两个字符串的删除操作_2.py
@@ -1,17 +1,18 @@
+#思路二
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         memo = [[0 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
-        res = 0
+        for i in range(len(word1) + 1):
+            memo[i][0] = i
+        for j in range(len(word2) + 1):
+            memo[0][j] = j
         for i in range(1, len(word1) + 1):
             for j in range(1, len(word2) + 1):
                 if word1[i - 1] == word2[j - 1]:
-                    memo[i][j] = memo[i - 1][j - 1] + 1
+                    memo[i][j] = memo[i - 1][j - 1]
                 else:
-                    memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
-
-                if memo[i][j] > res:
-                    res = memo[i][j]
-        return len(word1) - res + len(word2) - res
+                    memo[i][j] = min(memo[i - 1][j] + 1, memo[i][j - 1] + 1)
+        return memo[-1][-1]
 
 
 def main():
