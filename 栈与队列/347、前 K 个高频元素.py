818e0477e8b17b2fd5cb7f702d6167c94828999f
Jintaimeng <929357244@qq.com>
2023-08-19 22:42:25 +0800

    347

--- 栈与队列/347、前 K 个高频元素.py
+++ 栈与队列/347、前 K 个高频元素.py
@@ -1,9 +1,24 @@
+import heapq
 from typing import List
 
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
+        fre = {}
+        for i in range(len(nums)):
+            if nums[i] in fre.keys():
+                fre[nums[i]] += 1
+            else:
+                fre[nums[i]] = 1
+        priQue = []
+        for n, f in fre.items():
+            heapq.heappush(priQue, (f, n))
+            if len(priQue) > k:
+                heapq.heappop(priQue)
+        res = []
+        for i in range(len(priQue)):
+            res.append(priQue[i][1])
+        return res
 
 
 def main():
