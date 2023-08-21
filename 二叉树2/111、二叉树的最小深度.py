e6a587aa79c4de77e185c74edf19ce4cb903b676
Jintaimeng <929357244@qq.com>
2023-08-21 09:26:58 +0800

    111

---
+++ 二叉树2/111、二叉树的最小深度.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        elif root.left is None and root.right is None:
+            return 1
+        elif root.left and root.right:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        elif root.left:
+            return self.minDepth(root.left) + 1
+        else:
+            return self.minDepth(root.right) + 1
