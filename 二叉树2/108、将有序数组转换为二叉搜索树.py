d46ce4f09ff03c0e520ed845ee0ea260e2518e8a
Jintaimeng <929357244@qq.com>
2023-08-27 10:04:12 +0800

    108

---
+++ 二叉树2/108、将有序数组转换为二叉搜索树.py
@@ -0,0 +1,19 @@
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if len(nums) == 0:
+            return None
+        index = (len(nums) - 1) // 2
+        root = TreeNode(nums[index])
+        root.left = self.sortedArrayToBST(nums[: index])
+        root.right = self.sortedArrayToBST(nums[index + 1: ])
+        return root
