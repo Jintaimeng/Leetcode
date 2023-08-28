dfa4c91e8fabcc3b3bce696bcf9803b70fac877a
Jintaimeng <929357244@qq.com>
2023-08-28 10:34:31 +0800

    77剪枝

---
+++ 回溯算法/77、组合_剪枝.py
@@ -0,0 +1,30 @@
+#剪枝优化
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.res = []
+        self.temp = []
+        self.getCombine(n, k, 1)
+        return self.res
+
+    def getCombine(self, n, k, begin):
+        if len(self.temp) == k:
+            self.res.append(self.temp[: ])
+            return
+        for i in range(begin, n - (k - len(self.temp)) + 2):
+            self.temp.append(i)
+            self.getCombine(n, k, i + 1)
+            self.temp.pop()
+
+
+def main():
+    n = 4
+    k = 2
+    res = Solution().combine(n, k)
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
