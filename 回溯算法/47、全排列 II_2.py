97491c7c8a686206d053834b63952e66685b3bb5
Jintaimeng <929357244@qq.com>
2023-08-30 11:13:30 +0800

    4747_2

--- 回溯算法/47、全排列 II_2.py
+++ 回溯算法/47、全排列 II_2.py
@@ -5,25 +5,27 @@ class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = []
         self.used = [False for _ in range(len(nums))]
-        self.backtracking(sorted(nums), [], res)
+        self.backtracking(nums, [], res)
         return res
 
     def backtracking(self, nums, path, res):
         if len(path) == len(nums):
             res.append(path[:])
             return
+        use = set()
         for i in range(len(nums)):
-            if (i > 0 and nums[i] == nums[i - 1] and not self.used[i - 1]) or self.used[i]:
+            if self.used[i] or nums[i] in use:
                 continue
             path.append(nums[i])
             self.used[i] = True
+            use.add(nums[i])
             self.backtracking(nums, path, res)
-            self.used[i] = False
             path.pop()
+            self.used[i] = False
 
 
 def main():
-    nums = [1,1,2]
+    nums = [1,2,3]
     res = Solution().permuteUnique(nums)
     print(res)
 
