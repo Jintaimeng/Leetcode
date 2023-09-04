412ee2834f06bd30d6a628a1caa5d4f7fa935e1c
Jintaimeng <929357244@qq.com>
2023-09-04 19:44:51 +0800

    142

---
+++ 二、链表/142、环形链表 II.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                index1 = head
+                index2 = slow
+                while index1 != index2:
+                    index1 = index1.next
+                    index2 = index2.next
+                return index1
+        return None
